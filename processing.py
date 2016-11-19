c0da72d902f9c80e1eb86761d85caacdfc98466a
aleker <aleker@users.noreply.github.com>
2016-11-19 16:50:07 +0100

    Adding colour detection (for red, blue and white colour). find_ribbon function not working yet.

--- processing.py
+++ processing.py
@@ -6,93 +6,82 @@ from matplotlib import pyplot as plt
 import computing
 import argparse
 
-name = ''
+
+name2 = ''
 
 
-def make_decision(image, image_name):
-	my_decision = computing.DecisionStruct(name_of_file=image_name, month=5, pkt=2)
-
-	# CHECK DECISION:
-	if my_decision.name_of_file == None:
-		print ("DECISION", image_name, "\tNO DECISION")
-		return False
-	print("DECISION\t", my_decision.name_of_file, my_decision.month, my_decision.pkt)
-	for card in computing.facts_dictionary.items():
-		if card[0] == my_decision.name_of_file:		# card as list [key, [month, pkt]]
-			print("FACT\t\t", card[0], card[1][0], card[1][1])
-			if card[1][0] != my_decision.month:
-				print("\t\tWrong month.")
-			if card[1][1] != my_decision.pkt:
-				print("\t\tWrong points.")
-			break
-	return my_decision
-
-
-def picture_processing(file_name):
+def picture_processing(file_path):
 	# READ IMAGE:
-	filename = os.path.join(os.getcwd(), file_name)
+	filename = os.path.join(os.getcwd(), file_path)
 	original_image = cv2.imread(filename, cv2.IMREAD_COLOR)
-	#image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)	# cv2.IMREAD_GRAYSCALE / IMREAD_COLOR
 	image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 
-	#-------------------------------------------------
 	# PROCESSING:
-	res = np.hstack ((image, image))
-
-	# blur: Bilateral filtering has the nice property of removing noise in
-	# 		the image while still preserving the actual edges.
-	#		it works similarly to beauty-filter
-	blurred = cv2.bilateralFilter(image, 11, 17, 17)
-	#blurred = cv2.GaussianBlur(image, (3,3), 0)
-	res = np.hstack ((image, blurred))
-
-	# clahe:
-	# clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-	# cl1 = clahe.apply(image)
-	# res = np.hstack((image,cl1))
-
-	# equalization:
-	# equ = cv2.equalizeHist(image)
-	# res = np.hstack((image, equ))
-
-	# canny: smallest <- edge linking, largest <- initial segments of strong edges
-	# edges = cv2.Canny(image, threshold1=200, threshold2=600, apertureSize=3)
-	edges = computing.auto_canny(blurred)
-
-	# contours:
-	(_, contours, _) = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	contours = sorted(contours, key=cv2.contourArea, reverse=True)[:6]
-	res = np.hstack ((original_image, cv2.drawContours(original_image.copy(), contours, -1, color=(0,255,0), thickness=3)))
-	# http://www.pyimagesearch.com/2014/04/21/building-pokedex-python-finding-game-boy-screen-step-4-6/
-
-	#hull = cv2.convexHull(edges)	# to z ręką http://docs.opencv.org/3.1.0/dd/d49/tutorial_py_contour_features.html
-
-
-	#--------------------------------------------------
-	# SAVE FILES:
-	output_path = os.path.join (os.getcwd(), "output/")
-	if not os.path.exists(output_path):
-		os.mkdir(output_path)
-	new_path = os.path.join(output_path, os.path.basename(file_name) + name)
-	cv2.imwrite(new_path, res)
-	new_path = os.path.join (output_path, os.path.basename (file_name) + "e.jpg" + name)
-	cv2.imwrite (new_path, edges)
+	# find ribbon:
+	(colours_array, is_ribbon) = find_ribbon(original_image, os.path.basename(file_path))
 
 	# DECISION:
-	decision = make_decision(edges, os.path.basename(filename))
+	# TODO wypełnianie struktury
+	summary = computing.DecisionStruct(os.path.basename(file_path), colours_array, isRibbon=is_ribbon)
+	make_decision(summary)
+
+
+def find_ribbon(image, file_name):
+	results_array, masks_array = computing.find_colour_count(image, file_name)
+	for n, mask in enumerate(masks_array[:2]):
+		red_picture = np.zeros(image.shape, np.uint8)
+		red_picture[:] = (0, 0, 255)
+		red_mask = cv2.bitwise_and (red_picture, red_picture, mask=mask)
+		edges = cv2.Canny (red_mask, threshold1=200, threshold2=600, apertureSize=3)
+
+		# rectangle and contours
+		# TODO wykrywanie wstążki -> prostokąt + kolor
+		(_, cnts, _) = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
+		ribbon = None
+		# loop over our contours
+		for c in cnts:
+			if len(c) > 100:
+				ribbon = c
+				break
+		# SHOW:
+		cv2.drawContours (red_mask, [ribbon], -1, (0, 255, 0), 3)
+		cv2.imshow('edges', red_mask)
+		cv2.waitKey (0)
+
+	return results_array, ribbon
+
+
+def make_decision(result_of_processing):
+	# TODO porównaj rezultaty i dopasuj
+	# COMPARE:
+	a = 3
+	# print(result_of_processing.name_of_file, "No decision")
 
 
 def read_pictures ():
-	files_list = glob.glob ("pictures/*.jpg")
+	files_list = sorted(glob.glob (computing.proces_input + "*.jpg"))
 	for n, file in enumerate(files_list):
-		if n == 5:
+		if n == 20:
 			break
 		if os.path.basename(file)[0].isdigit():
 			picture_processing(file)
 
 
 if __name__ == '__main__':
-	computing.read_information('pictures/')
+	computing.read_information(computing.info_input)
+	computing.compute_parameters()
 	read_pictures()
+
+
+def save_file (file_name, result, edges):
+	output_path = os.path.join (os.getcwd (), "output/")
+	if not os.path.exists (output_path):
+		os.mkdir (output_path)
+	new_path = os.path.join (output_path, os.path.basename (file_name) + name2)
+	cv2.imwrite (new_path, result)
+	new_path = os.path.join (output_path, os.path.basename (file_name) + "e.jpg" + name2)
+	cv2.imwrite (new_path, edges)
+
 
 # http://docs.opencv.org/3.1.0/dd/d49/tutorial_py_contour_features.html (7 dział - prostokąt)
