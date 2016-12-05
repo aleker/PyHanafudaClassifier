1191a51ecabc95360bb3ec29d3feacdef61f2162
aleker <aleker@users.noreply.github.com>
2016-12-05 22:04:22 +0100

    No rotation.

--- computing.py
+++ computing.py
@@ -4,9 +4,9 @@ import os
 import re
 import numpy as np
 from collections import namedtuple
+from tempfile import TemporaryFile
 
 import card
-
 
 name = ''
 reference_input = 'pictures/'
@@ -15,93 +15,98 @@ test_input = 'pictures_for_testing/'
 # { name_of_file : [month, pkt, [colours], [[hu_moments_red], [hu_moments_blue], [hu_moments_white]] }
 facts_dictionary = {}
 # cards_list = []
-colour_boundaries = [	# ([B,G,R], [B,G,R])
-	([4, 9, 86], [80, 88, 220]),		# RED
-	([80, 31, 4], [220, 100, 60]),		# BLUE
-	([230, 230, 230], [255, 255, 255])	# WHITE
+colour_boundaries = [  # ([B,G,R], [B,G,R])
+    ([4, 9, 86], [80, 88, 220]),  # RED
+    ([80, 31, 4], [220, 100, 60]),  # BLUE
+    ([230, 230, 230], [255, 255, 255])  # WHITE
 ]
 
 
 def read_information(folder):
-	files_list = sorted(glob.glob (folder + "*.jpg"))
-	for file in files_list:
-		file = os.path.basename(file)
-		if file[0].isdigit():
-			file_atribiutes = file.split('.')[0].split('-')[:2]
-			file_atribiutes[0] = int(float(file_atribiutes[0]))
-			file_atribiutes[1] = int(float(re.findall(r'\d+', file_atribiutes[1])[0]))
-			facts_dictionary[file] = file_atribiutes
-			# current_card = card.Card(file)
-			# current_card.month = file_atribiutes[0]
-			# current_card.points = file_atribiutes[1]
-			# cards_list.append(current_card)
+    files_list = sorted(glob.glob(folder + "*.jpg"))
+    for file in files_list:
+        file = os.path.basename(file)
+        if file[0].isdigit():
+            file_atribiutes = file.split('.')[0].split('-')[:2]
+            file_atribiutes[0] = int(float(file_atribiutes[0]))
+            file_atribiutes[1] = int(float(re.findall(r'\d+', file_atribiutes[1])[0]))
+            facts_dictionary[file] = file_atribiutes
+        # current_card = card.Card(file)
+        # current_card.month = file_atribiutes[0]
+        # current_card.points = file_atribiutes[1]
+        # cards_list.append(current_card)
+    print('REFERENCE pictures count:', len(facts_dictionary))
 
 
-def find_colour_count(image, file_name):
-	colourful_count = []
-	masks = []
-	for n, (lower, upper) in enumerate (colour_boundaries):
-		lower = np.array (lower, dtype="uint8")
-		upper = np.array (upper, dtype="uint8")
+def find_colour_count(image, file_name = None):
+    colourful_count = []
+    masks = []
+    for n, (lower, upper) in enumerate(colour_boundaries):
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
 
-		mask = cv2.inRange (image, lower, upper)
-		kernel = np.ones((3,3), np.uint8)
-		mask2 = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-		mask = mask2
+        mask = cv2.inRange(image, lower, upper)
+        kernel = np.ones((3, 3), np.uint8)
+        mask2 = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = mask2
 
-		colourful_pixels_count = cv2.countNonZero (mask)
-		colourful_count.append(colourful_pixels_count)
-		masks.append(mask)
-		# print(file_name, n, colourful_pixels_count)
+        colourful_pixels_count = cv2.countNonZero(mask)
+        colourful_count.append(colourful_pixels_count)
+        masks.append(mask)
 
-		# SAVE MASK:
-		# image_with_mask = cv2.bitwise_and (image, image, mask=mask)
-		# save_file(file_name + name, "ribbons_masks/"+str(n)+"/", image_with_mask)
+        if file_name is not None:
+            print(file_name, n, colourful_pixels_count)
+            # SAVE MASK:
+            image_with_mask = cv2.bitwise_and (image, image, mask=mask)
+            save_file(file_name + name, "ribbons_masks/"+str(n)+"/", image_with_mask)
 
-	return colourful_count, masks
+    return colourful_count, masks
 
-def computeHuMoments (image, file_name):
-	results_array, masks_array = find_colour_count (image, file_name)
-	hu_moments_for_all_colours = []
-	for n, mask in enumerate (masks_array[:]):
-		# white mask
-		height, width = image.shape[:2]
-		red_picture = np.zeros (image.shape, np.uint8)
-		red_picture[:] = (255, 255, 255)
-		white_mask = cv2.bitwise_and (red_picture, red_picture, mask=mask)
 
-		# hu moments - silhouette
-		hu_moments = cv2.HuMoments(cv2.moments(cv2.cvtColor(white_mask, cv2.COLOR_BGR2GRAY))).flatten()
-		hu_moments_for_all_colours.append(hu_moments)
-		# print (file_name, n, "\n", hu_moments)
+def computeHuMoments(image, file_name = None):
+    results_array, masks_array = find_colour_count(image, file_name)
+    hu_moments_for_all_colours = []
+    for n, mask in enumerate(masks_array[:]):
+        # white mask
+        height, width = image.shape[:2]
+        red_picture = np.zeros(image.shape, np.uint8)
+        red_picture[:] = (255, 255, 255)
+        white_mask = cv2.bitwise_and(red_picture, red_picture, mask=mask)
 
-		# SAVE:
-		save_file (file_name, "hu_moments/" + str (n) + '/', white_mask)
+        # hu moments - silhouette
+        hu_moments = cv2.HuMoments(cv2.moments(cv2.cvtColor(white_mask, cv2.COLOR_BGR2GRAY))).flatten()
+        hu_moments_for_all_colours.append(hu_moments)
+        # print (file_name, n, "\n", hu_moments)
 
-	return hu_moments_for_all_colours
+        # SAVE:
+        if file_name is not None:
+            save_file(file_name, "hu_moments/" + str(n) + '/', white_mask)
+
+    return hu_moments_for_all_colours
 
 
 def compute_parameters():
-	for file_key in sorted (facts_dictionary.keys ()):
-		# READ FILE:
-		filename = os.path.join (os.getcwd (), reference_input + file_key)
-		original_image = cv2.imread (filename, cv2.IMREAD_COLOR)
-		image = cv2.cvtColor (original_image, cv2.COLOR_BGR2GRAY)
+    for file_key in sorted(facts_dictionary.keys()):
+        # READ FILE:
+        filename = os.path.join(os.getcwd(), reference_input + file_key)
+        original_image = cv2.imread(filename, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 
-		# COMPUTE PARAMETERS:
-		# colour count:
-		(array_of_colour_values, _) = find_colour_count (original_image, file_key)
-		facts_dictionary[file_key].append (array_of_colour_values)
+        # COMPUTE PARAMETERS:
+        # colour count:
+        (array_of_colour_values, _) = find_colour_count(original_image, file_key)
+        facts_dictionary[file_key].append(array_of_colour_values)
 
-		# hu_moments:
-		# TODO zmienić żeby dwa razy nie obliczał kolorów
-		hu_moments_for_all_colours = computeHuMoments(original_image, file_key)
-		facts_dictionary[file_key].append(hu_moments_for_all_colours)
+        # hu_moments:
+        # TODO zmienić żeby dwa razy nie obliczał kolorów
+        hu_moments_for_all_colours = computeHuMoments(original_image, file_key)
+        facts_dictionary[file_key].append(hu_moments_for_all_colours)
+    print("REFERENCE pictures computed.")
 
 
-def save_file (file_name, output_folder, result):
-	output_path = os.path.join (os.getcwd (), output_folder)
-	if not os.path.exists (output_path):
-		os.makedirs(output_path)
-	new_path = os.path.join (output_path, file_name + name)
-	cv2.imwrite (new_path, result)
+def save_file(file_name, output_folder, result):
+    output_path = os.path.join(os.getcwd(), output_folder)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    new_path = os.path.join(output_path, file_name + name)
+    cv2.imwrite(new_path, result)
--- processing.py
+++ processing.py
@@ -13,43 +13,31 @@ def picture_processing(file_path):
     # READ IMAGE:
     filename = os.path.join(os.getcwd(), file_path)
     original_image = cv2.imread(filename, cv2.IMREAD_COLOR)
-    gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-
-    card_list = []
-
-    current_card = card.Card(os.path.basename(file_path))
 
     # PROCESSING:
-    # find rectangle (card contour):
-    findCardShape(original_image, os.path.basename(file_path))
+    # scale image (max width(height) is 500):
+    original_image = scaleImage(original_image)
+    gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 
-    # compute hu moments:
+    # find cards on picture:
+    card_list = findCardShape(original_image.copy(), os.path.basename(file_path))
 
-    # find ribbon:
-    #(current_card.colours_count_array, current_card.isRibbon) = findRibbon(original_image, os.path.basename(file_path))
+    for n, one_card_contour in enumerate(card_list):
+        current_card = card.Card(str(n) + os.path.basename(file_path))
+        # take out card_picture from image:
+        card_picture = cropCardFromPicture(one_card_contour, original_image.copy())
+        # compute hu moments:
+        current_card.huMoments = computing.computeHuMoments(card_picture.copy())
+        # find ribbon:
+        (current_card.colours_count_array, current_card.isRibbon) = findRibbon(card_picture, str(n) + os.path.basename(file_path))
 
-    # DECISION:
-    makeDecision(current_card)
-
-
-def is_close(beginning, ending):
-    boundary = 30
-    if (beginning[0] > ending[0] - (beginning[0] > (ending[0] + boundary))).all:
-        return False
-    elif (beginning[0] < ending[0] - (beginning[0] + boundary < ending[0])).all:
-        return False
-    elif (beginning[1] > ending[1] - (beginning[1] > (ending[1] + boundary))).all:
-        return False
-    elif (beginning[1] < ending[1] - (beginning[1] + boundary < ending[1])).all:
-        return False
-    else:
-        return True
+        # DECISION:
+        makeDecision(current_card)
 
 
-def findCardShape(colour_image, filename):
-    gray_image = cv2.cvtColor(colour_image, cv2.COLOR_BGR2GRAY)
+def scaleImage(image):
     # SCALING:
-    height, width = gray_image.shape[:2]
+    height, width = image.shape[:2]
     max_width = 500.0
     if height > width:
         coefficient = height / max_width
@@ -59,8 +47,45 @@ def findCardShape(colour_image, filename):
         coefficient = width / max_width
         new_width = int(max_width)
         new_height = int(height / coefficient)
-    gray_image = cv2.resize(gray_image, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
-    colour_image = cv2.resize(colour_image, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+    image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+    return image
+
+
+def cropCardFromPicture(contour_of_card, image):
+    height, width = image.shape[:2]
+    # compute angle of contour
+    (x, y), (MA, ma), angle = cv2.fitEllipse(contour_of_card)
+    angle_to_full = -180.0 + angle
+    # compute the center of the contour
+    M = cv2.moments(contour_of_card)
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    # rotate
+    M = cv2.getRotationMatrix2D((cX, cY), angle_to_full, 1)
+    dst = cv2.warpAffine(image, M, (width, height))
+
+    leftmost = tuple(contour_of_card[contour_of_card[:,:,0].argmin()][0])
+    rightmost = tuple(contour_of_card[contour_of_card[:,:,0].argmax()][0])
+    topmost = tuple(contour_of_card[contour_of_card[:,:,1].argmin()][0])
+    bottommost = tuple(contour_of_card[contour_of_card[:,:,1].argmax()][0])
+    width_of_contour = abs(rightmost[0] - leftmost[0])
+    height_of_contour = abs(bottommost[1] - topmost[1])
+
+    x_difference = int((np.argmin([topmost[0], bottommost[0]]) - leftmost[0]) / 2)
+    y_difference = int((np.argmin([leftmost[1], rightmost[1]]) - topmost[1]) / 2)
+
+    #cropped = dst[(topmost[1] + y_difference):(bottommost[1] - y_difference), (leftmost[0] + x_difference):(rightmost[0] - x_difference)]     # [startY:endY , startX:endX]
+    cropped = image[topmost[1]:bottommost[1], leftmost[0]:rightmost[0]]  # [startY:endY , startX:endX]
+
+    # Show the output image
+    # cv2.imshow('Output', cropped)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return cropped
+
+
+def findCardShape(colour_image, filename):
+    gray_image = cv2.cvtColor(colour_image, cv2.COLOR_BGR2GRAY)
     # CANNY:
     image = cv2.GaussianBlur(gray_image, (5, 5), 0)
     edges = cv2.Canny(image, threshold1=500, threshold2=1100, apertureSize=5)
@@ -79,16 +104,20 @@ def findCardShape(colour_image, filename):
         approx = cv2.approxPolyDP(c, 0.02 * peri, True)
         if len(approx) == 4 and cv2.contourArea(c) > 700:      # [Next, Previous, First_child, Parent] hierarchy[0][n][3] == -1
             cards.append(c)
+
     # SAVING:
     # computing.save_file('e' + filename, 'testing_output/', edge_dilation)
     cv2.drawContours(colour_image, cards, -1, (0, 255, 0), 3)
     computing.save_file(filename, '1_find_shape_of_card/', colour_image)
     # print(filename, len(cnts), len(cards))
 
+    return cards
+
 
 def findRibbon(image, file_name):
-    org = image
-    results_array, masks_array = computing.find_colour_count(image, file_name)
+    width, height = image.shape[:2]
+    print('wh: ', width, height)
+    results_array, masks_array = computing.find_colour_count(image.copy(), file_name)
     ribbon_colour = None
     for n, mask in enumerate(masks_array[:2]):
         # TODO one more ribbon
@@ -117,16 +146,15 @@ def findRibbon(image, file_name):
 
         # SAVE:
         cv2.drawContours(red_mask, [ribbon], -1, (0, 255, 0), 3)
-        computing.save_file(file_name, "ribbons_contours/" + str(n) + '/', red_mask)
+        computing.save_file(file_name, "ribbons_contours/" + str(n) + '/', image)
 
     return results_array, ribbon_colour
 
 
 def makeDecision(card):
     # TODO compare results and make a decision
-    # COMPARE:
-    a = 3
-# print(result_of_processing.name_of_file, "No decision")
+    print('CARD: ', card.name_of_file)
+    print('RIBBON: ', card.isRibbon)
 
 
 def read_pictures():
@@ -134,12 +162,11 @@ def read_pictures():
     files_list = []
     for type in types:
         files_list.extend(sorted(glob.glob(computing.test_input + type)))
-    print('liczba plików do przetworzenia: ', len(files_list))
     for n, file in enumerate(files_list):
         # if n == 2:
         #  	break
-        #if os.path.basename(file)[0].isdigit():
         picture_processing(file)
+    print('TESTING pictures count:', len(files_list))
 
 
 if __name__ == '__main__':
@@ -148,7 +175,5 @@ if __name__ == '__main__':
     computing.compute_parameters()
 
     # TESTED PICTURES:
-    read_pictures()
+    #read_pictures()
 
-
-    # http://docs.opencv.org/3.1.0/dd/d49/tutorial_py_contour_features.html (7 dział - prostokąt)
